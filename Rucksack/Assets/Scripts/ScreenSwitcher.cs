0fadbe48a375439f718eb3a2567bfae0dc1eaf89
Nikolas <nweber3@hs-mittweida.de>
2020-12-16 12:28:10 +0100

    Hauptmenü + Navigation
    Hauptmenü + restliche Szenen hinzugefügt. Navigation zwischen den Szenen hinzugefügt. Beenden-Button funktioniert nicht, wenn über Unity gestartet. Hoffentlich funktioniert .gitignore.

---
+++ Rucksack/Assets/Scripts/ScreenSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenSwitcher : MonoBehaviour
+{
+    public void toMain() {
+        SceneManager.LoadScene("Hauptmenu");
+    }
+
+    public void toTut() {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void toExcer() {
+        SceneManager.LoadScene("Ubung");
+    }
+
+    public void toCredits() {
+        SceneManager.LoadScene("Mitwirkende");
+    }
+
+    public void toWiki() {
+        SceneManager.LoadScene("Wiki");
+    }
+
+    public void quit() {
+        Application.Quit();
+    }
+}
