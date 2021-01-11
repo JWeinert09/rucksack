983203c09518571612a5672578f9fd13b93cf8ce
Nikolas <nweber3@hs-mittweida.de>
2021-01-11 12:40:20 +0100

    Einbau Hintergrund
    Einbau der Hintergrundgrafik in Hauptmenü, Demo und Try it yourself

---
+++ Rucksack/Assets/Scripts/DemoControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void start() {
+        Debug.Log("Start");
+    }
+
+    public void stop() {
+        Debug.Log("Stop");
+    }
+
+    public void pause() {
+        Debug.Log("Pause");
+    }
+}
