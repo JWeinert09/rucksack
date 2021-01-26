403a5b3b896be47470f736143f298cd02c349165
Nikolas <nweber3@hs-mittweida.de>
2021-01-26 12:38:40 +0100

    DIY + Wiki
    Überarbeitung des Textes der Elster in der Übung, damit diese auf das Maximalgewicht hinweist. Überarbeitung des Textes der Wiki und hinzufügen der Buttons für das Öffnen der Links.

---
+++ Rucksack/Assets/Scripts/OpenLinks.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLinks : MonoBehaviour {
+    public bool link1;
+    public bool link2;
+    public bool link3;
+    public bool link4;
+    public bool link5;
+    public bool link6;
+
+     void OnMouseUp(){
+        if(link1) {
+            this.openlink1();
+        }
+        if(link2) {
+            this.openlink2();
+        }
+       if(link3) {
+            this.openlink3();
+        }
+        if(link4) {
+            this.openlink4();
+        }
+        if(link5) {
+            this.openlink5();
+        }
+        if(link6) {
+            this.openlink6();
+        }
+    }
+
+    public void openlink1() {
+        Debug.Log("link");
+        Application.OpenURL("https://www.fastleansmart.com/blog/das-rucksackproblem/");
+    }
+
+    public void openlink2() {
+        Application.OpenURL("https://www.bigdata-insider.de/der-greedy-algorithmus-a-843043/");
+    }
+
+     public void openlink3() {
+        Application.OpenURL("http://www.tigerjython.ch/download/rucksack.pdf");
+    }
+
+     public void openlink4() {
+        Application.OpenURL("https://www.uni-trier.de/fileadmin/fb4/prof/INF/TIN/Folien/GTI_III/ss_2010/VL8.pdf");
+    }
+
+     public void openlink5() {
+        Application.OpenURL("https://www.youtube.com/watch?v=hs6MNddIUtw");
+    }
+
+     public void openlink6() {
+        Application.OpenURL("https://www.youtube.com/watch?v=wMVf8lOn-Lc");
+    }    
+}
